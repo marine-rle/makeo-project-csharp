c0f62c67ea7a88c14f182cc14a523cad5b9d61e0
marine-rle <120376794+marine-rle@users.noreply.github.com>
2024-06-08 20:05:19 +0200

    Modification package

--- MakeoProject.DbLib/Class/MakeoProjectContext.cs
+++ MakeoProject.DbLib/Class/MakeoProjectContext.cs
@@ -47,11 +47,19 @@ public partial class MakeoProjectContext : DbContext
 
     public virtual DbSet<User> Users { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    /*        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
         => optionsBuilder.UseMySQL("server=192.168.56.56;port=3306;database=makeo_project;user=homestead;password=secret");
 
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    */
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        string connectionString = "server=10.193.55.1;port=3306;database=makeo_project;user=marine;password=M0812";
+        optionsBuilder.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+    }
+
+protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Cache>(entity =>
         {
